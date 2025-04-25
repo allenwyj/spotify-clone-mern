c78996cefb40e2880d8dfaf5b966212c98c52fe5
Yujie Wu <allenwuyujie@gmail.com>
2025-04-25 17:56:59 +1000

    bug fix

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -84,7 +84,7 @@ export const deleteSong = async (req, res, next) => {
 
 export const createAlbum = async (req, res, next) => {
   try {
-    const { title, artist, releaseYear } = res.body;
+    const { title, artist, releaseYear } = req.body;
     const { imageFile } = req.files;
 
     const imageUrl = await uploadToCloudinary(imageFile);
